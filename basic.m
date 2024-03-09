019fb80a83858c28e9b6ed58d41fcd3a72b024f3
dzxion <30489987+dzxion@users.noreply.github.com>
2024-03-10 00:03:08 +0800

    temp

--- basic.m
+++ basic.m
@@ -10,11 +10,14 @@ w = {0.1,100};
 % C = 1*s^0;
 % T1 = L1/(1+L1);
 % T2 = L2/(1+L2);
-% G0 = 246.7/(s^2+15.71*s+246.7);
-% TD = 157.9/(s^2+25.13*s+157.9);
-% Gf = (s^2+15.34*s+201.6)/(s^2+25.56*s+201.6);
-% GT = TD*G0;
-% G0f = Gf*G0;
+G0 = 246.7/(s^2+15.71*s+246.7);
+TD = 157.9/(s^2+25.13*s+157.9);
+Gf = (s^2+15.34*s+201.6)/(s^2+25.56*s+201.6);
+GT = TD*G0;
+G0f = Gf*G0;
+eigg0 = eig(G0)
+eiggt = eig(GT)
+eigg0f = eig(G0f)
 % P1 = 987/(s^2+75.4*s+987);
 % P2 = (1421/(s^2+67.86*s+1421))*((0.01*s+1)/(0.03*s+1))*(1/(0.035*s+1));
 % [A,B,C,D] = tf2ss(246.7,[1,15.71,246.7]);
@@ -30,46 +33,46 @@ w = {0.1,100};
 % wn = wd/((1-ksi^2)^0.5);
 % G4 = wn^2/(s^2+2*ksi*wn*s+wn^2)
 
-A = [0,1;-246.7,-15.71];
-B = [0;246.7];
-C = [1,0];
-D = 0;
-sys1 = ss(A,B,C,D)
-[b,a] = ss2tf(A,B,C,D)
-eig1 = eig(A)
-hinfnorm1 = hinfnorm(sys1)
-
-% A = [0,1;-10,-15];
-% B = [0;10];
+% A = [0,1;-246.7,-15.71];
+% B = [0;246.7];
 % C = [1,0];
 % D = 0;
-% sys2 = ss(A,B,C,D)
+% sys1 = ss(A,B,C,D)
 % [b,a] = ss2tf(A,B,C,D)
-% eig2 = eig(A)
-% hinfnorm2 = hinfnorm(sys2)
-
-A = [0,1;-987,-75.4];
-B = [0;987];
-C = [1,0];
-D = 0;
-sys3 = ss(A,B,C,D)
-[b,a] = ss2tf(A,B,C,D)
-eig3 = eig(A)
-hinfnorm3 = hinfnorm(sys3)
-
-A = [0,1;-35890,-37.89];
-B = [0;35890];
-C = [1,0];
-D = 0;
-sys4 = ss(A,B,C,D)
-[b,a] = ss2tf(A,B,C,D)
-eig4 = eig(A)
-hinfnorm4 = hinfnorm(sys4)
-
-figure
-step(sys1,sys3,sys4)
-figure
-bode(sys1,sys3,sys4)
+% eig1 = eig(A)
+% hinfnorm1 = hinfnorm(sys1)
+% 
+% % A = [0,1;-10,-15];
+% % B = [0;10];
+% % C = [1,0];
+% % D = 0;
+% % sys2 = ss(A,B,C,D)
+% % [b,a] = ss2tf(A,B,C,D)
+% % eig2 = eig(A)
+% % hinfnorm2 = hinfnorm(sys2)
+% 
+% A = [0,1;-987,-75.4];
+% B = [0;987];
+% C = [1,0];
+% D = 0;
+% sys3 = ss(A,B,C,D)
+% [b,a] = ss2tf(A,B,C,D)
+% eig3 = eig(A)
+% hinfnorm3 = hinfnorm(sys3)
+% 
+% A = [0,1;-35890,-37.89];
+% B = [0;35890];
+% C = [1,0];
+% D = 0;
+% sys4 = ss(A,B,C,D)
+% [b,a] = ss2tf(A,B,C,D)
+% eig4 = eig(A)
+% hinfnorm4 = hinfnorm(sys4)
+% 
+% figure
+% step(sys1,sys3,sys4)
+% figure
+% bode(sys1,sys3,sys4)
 
 % bode(L1);
 % bode(L2);
@@ -101,15 +104,15 @@ bode(sys1,sys3,sys4)
 % legend('TD');
 % grid;
 % 
-% figure;
-% bode(G0,GT,G0f,w);
-% legend('G0','GT','G0f');
-% grid;
-% % 
-% figure;
-% step(G0,GT,G0f);
-% legend('G0','GT','G0f');
-% grid;
+figure;
+bode(G0,GT,G0f,w);
+legend('G0','GT','G0f');
+grid;
+% 
+figure;
+step(G0,GT,G0f);
+legend('G0','GT','G0f');
+grid;
 
 % figure;
 % bode(P1,P2);
