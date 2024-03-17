62a1a731031d2ee9b18001aa2fee11c75fa44832
dzxion <30489987+dzxion@users.noreply.github.com>
2024-03-18 00:04:22 +0800

    temp

--- basic.m
+++ basic.m
@@ -2,22 +2,24 @@ close all
 clear
 clc
 
+%% transfer function
 s = tf('s');
 w = {0.1,100};
 
+% some practice
 % L1 = 3/(s*(0.1*s+1));
 % L2 = L1*(355/(s^2+1.1*s+355));
 % C = 1*s^0;
 % T1 = L1/(1+L1);
 % T2 = L2/(1+L2);
-G0 = 246.7/(s^2+15.71*s+246.7);
-TD = 157.9/(s^2+25.13*s+157.9);
-Gf = (s^2+15.34*s+201.6)/(s^2+25.56*s+201.6);
-GT = TD*G0;
-G0f = Gf*G0;
-eigg0 = eig(G0)
-eiggt = eig(GT)
-eigg0f = eig(G0f)
+% G0 = 246.7/(s^2+15.71*s+246.7);
+% TD = 157.9/(s^2+25.13*s+157.9);
+% Gf = (s^2+15.34*s+201.6)/(s^2+25.56*s+201.6);
+% GT = TD*G0;
+% G0f = Gf*G0;
+% eigg0 = eig(G0)
+% eiggt = eig(GT)
+% eigg0f = eig(G0f)
 % P1 = 987/(s^2+75.4*s+987);
 % P2 = (1421/(s^2+67.86*s+1421))*((0.01*s+1)/(0.03*s+1))*(1/(0.035*s+1));
 % [A,B,C,D] = tf2ss(246.7,[1,15.71,246.7]);
@@ -104,15 +106,15 @@ eigg0f = eig(G0f)
 % legend('TD');
 % grid;
 % 
-figure;
-bode(G0,GT,G0f,w);
-legend('G0','GT','G0f');
-grid;
-% 
-figure;
-step(G0,GT,G0f);
-legend('G0','GT','G0f');
-grid;
+% figure;
+% bode(G0,GT,G0f,w);
+% legend('G0','GT','G0f');
+% grid;
+% % 
+% figure;
+% step(G0,GT,G0f);
+% legend('G0','GT','G0f');
+% grid;
 
 % figure;
 % bode(P1,P2);
@@ -123,3 +125,82 @@ grid;
 % step(P1,P2);
 % legend('P1','P2');
 % grid;
+
+% cascade lead compensator
+G0 = 10/(s*(s+1));
+Gc = (1+0.456*s)/(1+0.114*s);
+Gc0 = Gc*G0
+figure
+bode(G0,Gc,Gc0)
+legend('G0','Gc','Gc0');
+
+%% state space
+% second order open loop system
+% without damping
+% A = [0 1;0 0];
+% B = [0;1];
+% C = [1 0];
+% D = 0;
+% G = ss(A,B,C,D);
+% eig = eig(A)
+% hinf = hinfnorm(G)
+% figure;
+% step(G)
+% figure;
+% bode(G)
+% x0 = [0;1];  % initial state
+% figure;
+% initial(G,x0)
+
+% second order open loop system
+% with damping
+% A = [0 1;0 -1];
+% B = [0;10];
+% C = [1 0];
+% D = 0;
+% G = ss(A,B,C,D);
+% eig = eig(A)
+% hinf = hinfnorm(G)
+% figure;
+% step(G)
+% figure;
+% bode(G)
+% x0 = [0;1];  % initial state
+% figure;
+% initial(G,x0)
+
+% second order close loop system 
+% error system 
+% output feedback
+% A = [0 1;-1 0];
+% B = [0;0];
+% C = [1 0];
+% D = 0;
+% G = ss(A,B,C,D);
+% eig = eig(A)
+% hinf = hinfnorm(G)
+% figure;
+% step(G)
+% figure;
+% bode(G)
+% x0 = [10;0];  % initial state
+% figure;
+% initial(G,x0)
+
+% second order close loop system 
+% error system 
+% state feedback
+% A = [0 1;-10 -10];
+% B = [0;0];
+% C = [1 0];
+% D = 0;
+% G = ss(A,B,C,D);
+% eig = eig(A)
+% hinf = hinfnorm(G)
+% figure;
+% step(G)
+% figure;
+% bode(G)
+% x0 = [1;0];  % initial state
+% figure;
+% initial(G,x0)
