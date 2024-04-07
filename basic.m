31a1caffaaf48bae19af4be8798aed386b9600ce
dzxion <30489987+dzxion@users.noreply.github.com>
2024-04-07 21:35:21 +0800

    temp

--- basic.m
+++ basic.m
@@ -2,9 +2,21 @@ close all
 clear
 clc
 
+%% some function
+% fplot(@(x) 20*log10(x))
+% f1 = @(x) 10*exp(-10*x);
+% f2 = @(x) 1*exp(-10*x);
+% fplot(@(x) 10*exp(-0*x))
+% fplot(@(x) 10*exp(-1*x),[0 5])
+% hold on
+% fplot(@(x) 5*exp(-1*x),[0 5])
+% hold on
+% fplot(@(x) 1*exp(-1*x),[0 5])
+
 %% transfer function
 s = tf('s');
 w = {0.1,100};
+syms k
 
 % some practice
 % L1 = 3/(s*(0.1*s+1));
@@ -17,17 +29,11 @@ w = {0.1,100};
 % Gf = (s^2+15.34*s+201.6)/(s^2+25.56*s+201.6);
 % GT = TD*G0;
 % G0f = Gf*G0;
-% eigg0 = eig(G0)
-% eiggt = eig(GT)
-% eigg0f = eig(G0f)
 % P1 = 987/(s^2+75.4*s+987);
 % P2 = (1421/(s^2+67.86*s+1421))*((0.01*s+1)/(0.03*s+1))*(1/(0.035*s+1));
 % [A,B,C,D] = tf2ss(246.7,[1,15.71,246.7]);
 % G1 = 1/(0.1*s+1);
 % % [A,B,C,D] = tf2ss(1,[0.1,1]);
-% ninfG0 = hinfnorm(G0);
-% ninfGT = hinfnorm(GT);
-% ninfG0f = hinfnorm(G0f);
 
 % f = 30;
 % ksi = 0.1;
@@ -41,8 +47,6 @@ w = {0.1,100};
 % D = 0;
 % sys1 = ss(A,B,C,D)
 % [b,a] = ss2tf(A,B,C,D)
-% eig1 = eig(A)
-% hinfnorm1 = hinfnorm(sys1)
 % 
 % % A = [0,1;-10,-15];
 % % B = [0;10];
@@ -50,8 +54,6 @@ w = {0.1,100};
 % % D = 0;
 % % sys2 = ss(A,B,C,D)
 % % [b,a] = ss2tf(A,B,C,D)
-% % eig2 = eig(A)
-% % hinfnorm2 = hinfnorm(sys2)
 % 
 % A = [0,1;-987,-75.4];
 % B = [0;987];
@@ -59,8 +61,6 @@ w = {0.1,100};
 % D = 0;
 % sys3 = ss(A,B,C,D)
 % [b,a] = ss2tf(A,B,C,D)
-% eig3 = eig(A)
-% hinfnorm3 = hinfnorm(sys3)
 % 
 % A = [0,1;-35890,-37.89];
 % B = [0;35890];
@@ -68,139 +68,53 @@ w = {0.1,100};
 % D = 0;
 % sys4 = ss(A,B,C,D)
 % [b,a] = ss2tf(A,B,C,D)
-% eig4 = eig(A)
-% hinfnorm4 = hinfnorm(sys4)
-% 
-% figure
-% step(sys1,sys3,sys4)
-% figure
-% bode(sys1,sys3,sys4)
-
-% bode(L1);
-% bode(L2);
-% bode(C);
-% bode(G0);
-
-% figure;
-% bode(L1,L2);
-% legend('L1','L2');
-% grid;
-% 
-% figure;
-% bode(T1,T2);
-% legend('T1','T2');
-% grid;
-% 
-% figure;
-% step(L1,L2);
-% legend('L1','L2');
-% grid;
-% 
-% figure;
-% step(T1,T2);
-% legend('T1','T2');
-% grid;
-
-% figure;
-% bode(TD);
-% legend('TD');
-% grid;
-% 
-% figure;
-% bode(G0,GT,G0f,w);
-% legend('G0','GT','G0f');
-% grid;
-% % 
-% figure;
-% step(G0,GT,G0f);
-% legend('G0','GT','G0f');
-% grid;
-
-% figure;
-% bode(P1,P2);
-% legend('P1','P2');
-% grid;
-% 
-% figure;
-% step(P1,P2);
-% legend('P1','P2');
-% grid;
 
 % cascade lead compensator
-G0 = 10/(s*(s+1));
-Gc = (1+0.456*s)/(1+0.114*s);
-Gc0 = Gc*G0
-figure
-bode(G0,Gc,Gc0)
-legend('G0','Gc','Gc0');
+% G0 = 10/(s*(s+1));
+% Gc = (1+0.456*s)/(1+0.114*s);
+% Gc0 = Gc*G0
 
 %% state space
-% second order open loop system
-% without damping
-% A = [0 1;0 0];
-% B = [0;1];
-% C = [1 0];
+% first order close loop system 
+% error system
+% C = [1];
 % D = 0;
-% G = ss(A,B,C,D);
-% eig = eig(A)
-% hinf = hinfnorm(G)
-% figure;
-% step(G)
-% figure;
-% bode(G)
-% x0 = [0;1];  % initial state
-% figure;
-% initial(G,x0)
+% 
+% k = 1;
+% A = [-k];
+% B = [k];
+% sys1 = ss(A,B,C,D);
+
+% k = 5;
+% A = [-k];
+% B = [k];
+% sys2 = ss(A,B,C,D);
+% 
+% k = 10;
+% A = [-k];
+% B = [k];
+% sys3 = ss(A,B,C,D);
+% 
+% k = 15;
+% A = [-k];
+% B = [k];
+% sys4 = ss(A,B,C,D);
+
+% bandwidth(sys1)
+% margin(sys1)
+% linearSystemAnalyzer(sys1)
 
 % second order open loop system
-% with damping
-% A = [0 1;0 -1];
-% B = [0;10];
-% C = [1 0];
-% D = 0;
-% G = ss(A,B,C,D);
-% eig = eig(A)
-% hinf = hinfnorm(G)
-% figure;
-% step(G)
-% figure;
-% bode(G)
-% x0 = [0;1];  % initial state
-% figure;
-% initial(G,x0)
+k = 2;
+A = [0 1;-k^2 -2*k];
+B = [0;0];
+C = [0 1];
+D = 0;
+sys2 = ss(A,B,C,D);
 
-% second order close loop system 
-% error system 
-% output feedback
-% A = [0 1;-1 0];
-% B = [0;0];
-% C = [1 0];
-% D = 0;
-% G = ss(A,B,C,D);
-% eig = eig(A)
-% hinf = hinfnorm(G)
-% figure;
-% step(G)
-% figure;
-% bode(G)
-% x0 = [10;0];  % initial state
-% figure;
-% initial(G,x0)
+A = [0 1;-1 -k];
+B = [0;0];
+C = [0 1];
+D = 0;
+sys3 = ss(A,B,C,D);
 
-% second order close loop system 
-% error system 
-% state feedback
-% A = [0 1;-10 -10];
-% B = [0;0];
-% C = [1 0];
-% D = 0;
-% G = ss(A,B,C,D);
-% eig = eig(A)
-% hinf = hinfnorm(G)
-% figure;
-% step(G)
-% figure;
-% bode(G)
-% x0 = [1;0];  % initial state
-% figure;
-% initial(G,x0)
