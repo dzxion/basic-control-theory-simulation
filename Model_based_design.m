9d39939980e620269142f805562d052aee625f96
dzxion <dengzhengxiong@xa.com>
2024-05-15 19:26:45 +0800

    temp

--- Model_based_design.m
+++ Model_based_design.m
@@ -3,7 +3,15 @@ clear
 clc
 
 s = tf('s');
-plant_velocity_dynamic = 1/s;
-plant_position_dynamic = 1/s;
-controller_velocity = tunablePID('controller_velocity','PI');
-controller_position = tunablePID('controller_position','P');
+P_vel = 1/s;
+P_pos = 1/s;
+C_vel = tunablePID('controller_velocity','PI');
+C_pos = tunablePID('controller_position','P');
+d_for = AnalysisPoint('d1');
+Vel_Loop = feedback(C_vel*d_for*P_vel,1);
+CL0 = feedback(C_pos*Vel_Loop*P_pos,1);
+CL0.InputName = 'r';
+CL0.OutputName = 'y';
+Rtrack = TuningGoal.Tracking('r','y',1,0.01);
+Rreject = TuningGoal.Gain('d1','y',0.1);
+[CL,fSoft] = systune(CL0,[Rtrack,Rreject]);
