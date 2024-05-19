6824141bd5d260083f1a67a9a2561d7f0aa6845f
dzxion <30489987+dzxion@users.noreply.github.com>
2024-05-19 23:00:42 +0800

    temp

--- Model_based_design.m
+++ Model_based_design.m
@@ -3,22 +3,70 @@ clear
 clc
 
 s = tf('s');
+
+%% single-loop
+% plant
 P_vel = 1/s;
-P_pos = 1/s;
-C_vel = tunablePID('controller_velocity','PI');
-C_pos = tunablePID('controller_position','P');
+% controller
+C_vel = tunablePID('controller_velocity','P');
+% analysis point
 d_for = AnalysisPoint('d1');
-Vel_Loop = feedback(C_vel*d_for*P_vel,1);
-CL0 = feedback(C_pos*Vel_Loop*P_pos,1);
+err = AnalysisPoint('e');
+% closed-loop system
+CL0 = feedback(err*C_vel*d_for*P_vel,1);
 CL0.InputName = 'r';
 CL0.OutputName = 'y';
-Rtrack = TuningGoal.Tracking('r','y',1,0.01);
-Rreject = TuningGoal.Gain('d1','y',0.1);
+% performance specifications
+% Rtrack = TuningGoal.Tracking('r','y',1,0.01);
+Rtrack = TuningGoal.StepTracking('r','y',0.05);
+% Rreject = TuningGoal.Gain('d1','e',0.1);
+Rreject = TuningGoal.StepRejection('d1','e',0.25,5);
+% tuning
 [CL,fSoft] = systune(CL0,[Rtrack,Rreject]);
+% [CL,fSoft] = systune(CL0,Rtrack);
+% validation
 figure
 stepplot(CL);
-CLdist = getIOTransfer(CL,'d1','y');
+CLdist = getIOTransfer(CL,'d1','e');
 figure
 stepplot(CLdist);
 figure
-viewGoal([Rtrack,Rreject],CL);
+viewGoal([Rtrack],CL);
+figure
+viewGoal([Rreject],CL);
+showTunable(CL);
+
+%% multi-loop
+% % plant
+% P_vel = 1/s;
+% P_pos = 1/s;
+% % controller
+% C_vel = tunablePID('controller_velocity','P');
+% C_pos = tunablePID('controller_position','P');
+% % analysis point
+% d_for = AnalysisPoint('d1');
+% err = AnalysisPoint('e');
+% % closed-loop system
+% Vel_Loop = feedback(C_vel*d_for*P_vel,1);
+% CL0 = feedback(err*C_pos*Vel_Loop*P_pos,1);
+% CL0.InputName = 'r';
+% CL0.OutputName = 'y';
+% % performance specifications
+% % Rtrack = TuningGoal.Tracking('r','y',1,0.01);
+% Rtrack = TuningGoal.StepTracking('r','y',0.05);
+% % Rreject = TuningGoal.Gain('d1','e',0.1);
+% % Rreject = TuningGoal.StepRejection('d1','e',0.25,5);
+% % tuning
+% % [CL,fSoft] = systune(CL0,[Rtrack,Rreject]);
+% [CL,fSoft] = systune(CL0,Rtrack);
+% % validation
+% figure
+% stepplot(CL);
+% CLdist = getIOTransfer(CL,'d1','e');
+% figure
+% stepplot(CLdist);
+% figure
+% viewGoal([Rtrack],CL);
+% % figure
+% % viewGoal([Rreject],CL);
+% showTunable(CL);
