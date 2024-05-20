efa37ae497f59d2f0fca53d2d8cd6c91772ce605
dzxion <30489987+dzxion@users.noreply.github.com>
2024-05-20 22:54:23 +0800

    temp

--- Model_based_design.m
+++ Model_based_design.m
@@ -4,39 +4,41 @@ clc
 
 s = tf('s');
 
-%% single-loop
-% plant
-P_vel = 1/s;
-% controller
-C_vel = tunablePID('controller_velocity','P');
-% analysis point
-d_for = AnalysisPoint('d1');
-err = AnalysisPoint('e');
-% closed-loop system
-CL0 = feedback(err*C_vel*d_for*P_vel,1);
-CL0.InputName = 'r';
-CL0.OutputName = 'y';
-% performance specifications
-% Rtrack = TuningGoal.Tracking('r','y',1,0.01);
-Rtrack = TuningGoal.StepTracking('r','y',0.05);
-% Rreject = TuningGoal.Gain('d1','e',0.1);
-Rreject = TuningGoal.StepRejection('d1','e',0.25,5);
-% tuning
-[CL,fSoft] = systune(CL0,[Rtrack,Rreject]);
+%% MATLAB
+% single-loop
+% % plant
+% P_vel = 1/s;
+% % controller
+% C_vel = tunablePID('controller_velocity','P');
+% % analysis point
+% d_for = AnalysisPoint('d1');
+% err = AnalysisPoint('e');
+% % closed-loop system
+% CL0 = feedback(err*C_vel*d_for*P_vel,1);
+% CL0.InputName = 'r';
+% CL0.OutputName = 'y';
+% % performance specifications
+% % Rtrack = TuningGoal.Tracking('r','y',1,0.01);
+% Rtrack = TuningGoal.StepTracking('r','y',0.05);
+% % Rreject = TuningGoal.Gain('d1','e',0.1);
+% % Rreject = TuningGoal.StepRejection('d1','e',0.25,5);
+% % tuning
+% % [CL,fSoft] = systune(CL0,[Rtrack,Rreject]);
 % [CL,fSoft] = systune(CL0,Rtrack);
-% validation
-figure
-stepplot(CL);
-CLdist = getIOTransfer(CL,'d1','e');
-figure
-stepplot(CLdist);
-figure
-viewGoal([Rtrack],CL);
-figure
-viewGoal([Rreject],CL);
-showTunable(CL);
+% % validation
+% figure
+% stepplot(CL);
+% CLdist = getIOTransfer(CL,'d1','e');
+% tf(CLdist)
+% figure
+% stepplot(CLdist);
+% figure
+% viewGoal([Rtrack],CL);
+% % figure
+% % viewGoal([Rreject],CL);
+% showTunable(CL);
 
-%% multi-loop
+% multi-loop
 % % plant
 % P_vel = 1/s;
 % P_pos = 1/s;
@@ -70,3 +72,13 @@ showTunable(CL);
 % % figure
 % % viewGoal([Rreject],CL);
 % showTunable(CL);
+
+%% SIMULINK
+sllin = slLinearizer('simple_model');
+addPoint(sllin,{'r','e','u','y','d'});
+CL = getIOTransfer(sllin,'r','y');
+figure
+stepplot(CL);
+CLu = getIOTransfer(sllin,'d','e');
+figure
+stepplot(CLu);
