663de8aaa7754c465682b7d1edcf9c4561fe27c8
dzxion <dengzhengxiong@xa.com>
2024-05-21 18:13:08 +0800

    temp

--- Model_based_design.m
+++ Model_based_design.m
@@ -4,31 +4,28 @@ clc
 
 s = tf('s');
 
-%% MATLAB
-% single-loop
+%% systune(有bug)
+% % single-loop
 % % plant
 % P_vel = 1/s;
 % % controller
-% C_vel = tunablePID('controller_velocity','P');
+% C_vel = tunablePID('C1','PI');
 % % analysis point
-% d_for = AnalysisPoint('d1');
+% d_for = AnalysisPoint('d');
 % err = AnalysisPoint('e');
 % % closed-loop system
 % CL0 = feedback(err*C_vel*d_for*P_vel,1);
 % CL0.InputName = 'r';
 % CL0.OutputName = 'y';
 % % performance specifications
-% % Rtrack = TuningGoal.Tracking('r','y',1,0.01);
 % Rtrack = TuningGoal.StepTracking('r','y',0.05);
-% % Rreject = TuningGoal.Gain('d1','e',0.1);
-% % Rreject = TuningGoal.StepRejection('d1','e',0.25,5);
+% % Rreject = TuningGoal.StepRejection('d','e',0.25,5);
 % % tuning
-% % [CL,fSoft] = systune(CL0,[Rtrack,Rreject]);
 % [CL,fSoft] = systune(CL0,Rtrack);
 % % validation
 % figure
 % stepplot(CL);
-% CLdist = getIOTransfer(CL,'d1','e');
+% CLdist = getIOTransfer(CL,'d','e');
 % tf(CLdist)
 % figure
 % stepplot(CLdist);
@@ -38,47 +35,29 @@ s = tf('s');
 % % viewGoal([Rreject],CL);
 % showTunable(CL);
 
-% multi-loop
-% % plant
-% P_vel = 1/s;
-% P_pos = 1/s;
-% % controller
-% C_vel = tunablePID('controller_velocity','P');
-% C_pos = tunablePID('controller_position','P');
-% % analysis point
-% d_for = AnalysisPoint('d1');
-% err = AnalysisPoint('e');
-% % closed-loop system
-% Vel_Loop = feedback(C_vel*d_for*P_vel,1);
-% CL0 = feedback(err*C_pos*Vel_Loop*P_pos,1);
-% CL0.InputName = 'r';
-% CL0.OutputName = 'y';
-% % performance specifications
-% % Rtrack = TuningGoal.Tracking('r','y',1,0.01);
-% Rtrack = TuningGoal.StepTracking('r','y',0.05);
-% % Rreject = TuningGoal.Gain('d1','e',0.1);
-% % Rreject = TuningGoal.StepRejection('d1','e',0.25,5);
-% % tuning
-% % [CL,fSoft] = systune(CL0,[Rtrack,Rreject]);
-% [CL,fSoft] = systune(CL0,Rtrack);
-% % validation
-% figure
-% stepplot(CL);
-% CLdist = getIOTransfer(CL,'d1','e');
-% figure
-% stepplot(CLdist);
-% figure
-% viewGoal([Rtrack],CL);
-% % figure
-% % viewGoal([Rreject],CL);
-% showTunable(CL);
+%% slTuner
+mdl = "simple_model";
+open_system(mdl)
+st0 = slTuner(mdl,"C1");
+addPoint(st0,["r","e","u","y","d"]);
 
-%% SIMULINK
-sllin = slLinearizer('simple_model');
-addPoint(sllin,{'r','e','u','y','d'});
-CL = getIOTransfer(sllin,'r','y');
+Rtrack = TuningGoal.StepTracking('r','y',0.05);
+Rreject = TuningGoal.StepRejection('d','e',0.25,5);
+[st,fSoft] = systune(st0,[Rtrack,Rreject]);
+
+CL = getIOTransfer(st,"r","y");
+tf(CL)
 figure
 stepplot(CL);
-CLu = getIOTransfer(sllin,'d','e');
+CLd2e = getIOTransfer(st,'d','e');
+tf(CLd2e)
 figure
-stepplot(CLu);
+stepplot(CLd2e);
+CLr2e = getIOTransfer(st,'r','e');
+figure
+stepplot(CLr2e);
+figure
+viewGoal([Rtrack],st);
+figure
+viewGoal([Rreject],st);
+showTunable(st);
