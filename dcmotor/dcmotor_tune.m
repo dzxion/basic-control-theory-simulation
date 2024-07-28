915b31e454cc27972f92157f8ce3049e44de132c
dzxion <30489987+dzxion@users.noreply.github.com>
2024-07-28 21:24:02 +0800

    temp

---
+++ dcmotor/dcmotor_tune.m
@@ -0,0 +1,20 @@
+close all
+clear
+clc
+
+mdl = "sl_dcmotor";
+open_system(mdl)
+st0 = slTuner(mdl,["C1"]);
+addPoint(st0,["r","w","u","w_f","e"]);
+% plant = getIOTransfer(st0,"u","w","w_f");
+% tf(plant)
+% 
+Req1 = TuningGoal.Tracking('r','w',0.5,0.05);
+Req2 = TuningGoal.Overshoot('r','w',10);
+Req3 = TuningGoal.Margins('e',5,40);
+[st,fSoft] = systune(st0,[Req1,Req2])
+getBlockValue(st,'C1')  % tuned value of PI controller
+CL = getIOTransfer(st,"r","w");
+tf(CL)
+figure
+stepplot(CL);
