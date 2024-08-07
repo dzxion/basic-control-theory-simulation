7811cee27e00d6aeb1022098ed0e515601761055
dzxion <30489987+dzxion@users.noreply.github.com>
2024-08-07 23:47:50 +0800

    temp

--- dcmotor/dcmotor_tune.m
+++ dcmotor/dcmotor_tune.m
@@ -10,16 +10,16 @@ clc
 % Phase margin greater than 40 degrees
 
 %% sl_dcmotor_pid
-mdl = "sl_dcmotor_pid";
-open_system(mdl)
-st0 = slTuner(mdl,["C1"]);
-addPoint(st0,["r","w","u","w_f","e"]);
-plant = getIOTransfer(st0,"u","w","w_f");
-tf(plant)
-Req1 = TuningGoal.Tracking('r','w',0.5,0.05);
-Req2 = TuningGoal.Overshoot('r','w',10);
-Req3 = TuningGoal.Margins('e',20,40);
-[st,fSoft] = systune(st0,[Req1,Req2,Req3])
+% mdl = "sl_dcmotor_pid";
+% open_system(mdl)
+% st0 = slTuner(mdl,["C1"]);
+% addPoint(st0,["r","w","u","w_f","e"]);
+% plant = getIOTransfer(st0,"u","w","w_f");
+% tf(plant)
+% Req1 = TuningGoal.Tracking('r','w',0.5,0.05);
+% Req2 = TuningGoal.Overshoot('r','w',10);
+% Req3 = TuningGoal.Margins('e',20,40);
+% [st,fSoft] = systune(st0,[Req1,Req2,Req3])
 
 %% sl_dcmotor_cascade_pid
 % mdl = "sl_dcmotor_cascade_pid";
@@ -34,6 +34,20 @@ Req3 = TuningGoal.Margins('e',20,40);
 % Req4 = TuningGoal.Margins('e_i',20,40);
 % Req4.Openings = 'w_f';
 % [st,fSoft] = systune(st0,[Req1,Req2,Req3,Req4])
+
+%% sl_dcmotor_cascade_adrc_pid
+mdl = "sl_dcmotor_cascade_adrc_pid";
+open_system(mdl)
+st0 = slTuner(mdl,["P1","C2"]);
+addPoint(st0,["r","w","u","w_f","e_w","i_f","e_i"]);
+plant = getIOTransfer(st0,"u","w",{"w_f","i_f"});
+tf(plant)
+Req1 = TuningGoal.Tracking('r','w',0.5,0.05);
+Req2 = TuningGoal.Overshoot('r','w',10);
+Req3 = TuningGoal.Margins('e_w',20,40);
+Req4 = TuningGoal.Margins('e_i',20,40);
+Req4.Openings = 'w_f';
+[st,fSoft] = systune(st0,[Req1,Req2,Req3,Req4])
 
 %% result
 showTunable(st)
