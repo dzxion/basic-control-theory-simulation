a2609ee061a7bae1ba79e1e310d301eaff696ca8
dzxion <30489987+dzxion@users.noreply.github.com>
2024-08-26 22:35:25 +0800

    temp

--- dcmotor/dcmotor_tune.m
+++ dcmotor/dcmotor_tune.m
@@ -119,16 +119,43 @@ clc
 % stepplot(CL);
 
 %% sl_dcmotor_cascade_pid_kalman (use kalman feedback)
+% mdl = "sl_dcmotor_cascade_pid_kalman";
+% open_system(mdl)
+% st0 = slTuner(mdl,["C1","C2","K1","K2","K3"]);
+% addPoint(st0,["r","p","u","w_f","e_w","p_f","e_p","p_hat"]);
+% 
+% U = realp('U', 1);% matrix U
+% Q = realp('Q', 1);% matrix Q
+% setBlockParam(st0,'K1',U);
+% setBlockParam(st0,'K2',Q);
+% setBlockParam(st0,'K3',Q);
+% 
+% Req1 = TuningGoal.Tracking('r','p',0.5,0.05);
+% Req2 = TuningGoal.Overshoot('r','p',10);
+% Req3 = TuningGoal.Margins('e_w',20,40);
+% Req4 = TuningGoal.Margins('e_p',20,40);
+% Req3.Openings = 'p_f';
+% [st,fSoft] = systune(st0,[Req1,Req2,Req3,Req4])
+% 
+% % writeBlockValue(st);
+% showTunable(st)
+% 
+% plant = getIOTransfer(st0,"u","p",{"w_f","p_f"})
+% tf(plant)
+% 
+% CL = getIOTransfer(st,"r","p")
+% tf(CL)
+% figure
+% stepplot(CL);
+% 
+% CL_KF = getIOTransfer(st,"p_f","p_hat")
+% tf(CL_KF)
+
+%% sl_dcmotor_cascade_pid_kalman (use Luenberger feedback)
 mdl = "sl_dcmotor_cascade_pid_kalman";
 open_system(mdl)
-st0 = slTuner(mdl,["C1","C2","K1","K2","K3"]);
+st0 = slTuner(mdl,["C1","C2","C3"]);
 addPoint(st0,["r","p","u","w_f","e_w","p_f","e_p","p_hat"]);
-
-U = realp('U', 1);% matrix U
-Q = realp('Q', 1);% matrix Q
-setBlockParam(st0,'K1',U);
-setBlockParam(st0,'K2',Q);
-setBlockParam(st0,'K3',Q);
 
 Req1 = TuningGoal.Tracking('r','p',0.5,0.05);
 Req2 = TuningGoal.Overshoot('r','p',10);
