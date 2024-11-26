a59f49a90e14603672f309503ecdafe55bbb023e
dzxion <30489987+dzxion@users.noreply.github.com>
2024-11-26 22:38:58 +0800

    temp

--- spmsm/init.m
+++ spmsm/init.m
@@ -2,22 +2,26 @@
 % clc
 
 % plant
-pa.Ls_q_H = 2.39e-3;
-pa.Ls_d_H = 2.10e-3;
-pa.Rs_Ohm = 5.2;
 pa.J = 0.02;
-pa.Kf = 0.2;
-pa.Km = .015;
-pa.Kb = .015;
+pa.B = 0.2;
+pa.Lq = 2.39e-3;
+pa.Ld = 2.10e-3;
+pa.phi_m = 1;
+pa.P = 14;
+% pa.Rs_Ohm = 5.2;
+% pa.J = 0.02;
+% pa.Kf = 0.2;
+% pa.Km = .015;
+% pa.Kb = .015;
 
 % controller
-pa.BWc_rps = 100;
-pa.CarrFreq = 20;
-currentCtrlPeriod_sec =  1.0 / (pa.CarrFreq * 1000);
-pa.Kp_Iq = pa.BWc_rps*pa.Ls_q_H;
-pa.Ki_Iq = pa.Rs_Ohm/pa.Ls_q_H;
-pa.Kp_Id = pa.BWc_rps*pa.Ls_d_H;
-pa.Ki_Id = pa.Rs_Ohm/pa.Ls_d_H;
+% pa.BWc_rps = 100;
+% pa.CarrFreq = 20;
+% currentCtrlPeriod_sec =  1.0 / (pa.CarrFreq * 1000);
+% pa.Kp_Iq = pa.BWc_rps*pa.Ls_q_H;
+% pa.Ki_Iq = pa.Rs_Ohm/pa.Ls_q_H;
+% pa.Kp_Id = pa.BWc_rps*pa.Ls_d_H;
+% pa.Ki_Id = pa.Rs_Ohm/pa.Ls_d_H;
 
 % pa.Kp_Id = 1;
 % pa.Ki_Id = 1;
