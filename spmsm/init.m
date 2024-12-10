f2277e46c01cf04ada5fb98b196b9faceee7d36d
dzxion <dengzhengxiong@xa.com>
2024-12-10 13:48:31 +0800

    temp
    1、使用Exercise6.11的参数

--- spmsm/init.m
+++ spmsm/init.m
@@ -2,16 +2,16 @@ clear
 clc
 
 % plant
-pa.J = 0.02;
-pa.B = 0.2;
-pa.Lq = 2.39e-3;
-pa.Ld = 2.10e-3;
+pa.J = 0.1;
+pa.B = 0.0001;
 pa.Lms = 0.3e-3; % gap crossing inductance
 pa.Lls = 0.02e-3;% leakage inductance
-
-pa.phi_m = 0.00469;
+pa.phi_m = 0.6;
 pa.P = 6;
 pa.R = 20e-3;
+
+pa.Lq = 2.39e-3;
+pa.Ld = 2.10e-3;
 % pa.Kf = 0.2;
 % pa.Km = .015;
 % pa.Kb = .015;
