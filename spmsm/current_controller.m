1a636ccc39d76707aa653123dd4b585761454e3f
dzxion <30489987+dzxion@users.noreply.github.com>
2024-12-17 23:06:05 +0800

    temp

--- spmsm/current_controller.m
+++ spmsm/current_controller.m
@@ -1,7 +1,7 @@
-function SPMSM_abc_model(block)
+function current_controller(block)
 % Level-2 MATLAB file S-Function.
 
-%   Copyright 1990-2009 The MathWorks, Inc.
+%   Copyright 1990-2009 The MathWorks, Inc.S
 
   setup(block);
   
@@ -19,20 +19,29 @@ function setup(block)
   %% inherited.
   block.SetPreCompInpPortInfoToDynamic;
   block.SetPreCompOutPortInfoToDynamic;
+  
+  block.InputPort(1).Dimensions        = [2,1];% idq_ref
+  block.InputPort(1).DirectFeedthrough = true;
  
-  block.InputPort(1).Dimensions        = [3,1];
-  block.InputPort(1).DirectFeedthrough = false;
+  block.InputPort(2).Dimensions        = [2,1];% idq_fed
+  block.InputPort(2).DirectFeedthrough = true;
   
-  block.InputPort(2).Dimensions        = [2,1];
-  block.InputPort(2).DirectFeedthrough = false;
+%   block.InputPort(3).Dimensions        = [3,1];% v
+%   block.InputPort(3).DirectFeedthrough = true;
+%   
+%   block.InputPort(4).Dimensions        = [3,3];% R
+%   block.InputPort(4).DirectFeedthrough = true;
   
-  block.OutputPort(1).Dimensions       = [3,1];
+%   block.InputPort(5).Dimensions        = [3,1];
+%   block.InputPort(5).DirectFeedthrough = true;
+  
+  block.OutputPort(1).Dimensions       = [2,1];
   
   %% Set block sample time to continuous
   block.SampleTimes = [0 0];
   
   %% Setup Dwork
-  block.NumContStates = 3;
+  block.NumContStates = 0;
   
   %% Set the block simStateCompliance to default (i.e., same as a built-in block)
   block.SimStateCompliance = 'DefaultSimState';
@@ -43,6 +52,7 @@ function setup(block)
   block.RegBlockMethod('Outputs',                 @Output);  
 %   block.RegBlockMethod('Update',                  @Update); 
   block.RegBlockMethod('Derivatives',             @Derivative);
+%   block.RegBlockMethod('SetInputPortSamplingMode',@SetInputPortSamplingMode);
   
 %endfunction
 
@@ -62,42 +72,33 @@ function InitConditions(block)
 
 %% Initialize Dwork
 %   block.Dwork(1).Data = block.DialogPrm(1).Data;
-block.ContStates.Data = [0;0;0];
+% block.ContStates.Data = [0;
+%                          0;
+%                          0;
+%                          0;
+%                          0;
+%                          0];
   
 %endfunction
 
 function Output(block)
 
-block.OutputPort(1).Data = block.ContStates.Data;
+idq_ref = block.InputPort(1).Data;
+idq_fed = block.InputPort(2).Data;
+pa = block.DialogPrm(1).Data;
+
+% J_hat = pa.J_hat;
+% K_w = pa.K_w;
+% Sw = skew_symmetric_matrix(omega);
+% 
+% Gamma = Sw*J_hat*omega_d - J_hat*K_w*(omega-omega_d);
+
+block.OutputPort(1).Data = udq;
   
 %endfunction
 
 function Derivative(block)
 
-vabc = block.InputPort(1).Data;
-theta_r = block.InputPort(2).Data(1);
-wr = block.InputPort(2).Data(2);
-x = block.ContStates.Data;
-pa = block.DialogPrm(1).Data;
-R = pa.R;
-P = pa.P;
-Lms = pa.Lms;
-Lls = pa.Lls;
-phi_m = pa.phi_m;
-we = P/2 * wr;
-theta_e = P/2 * theta_r;
-gamma = 2*Lls/Lms;
-Labc_inv = 2/(Lms*((2+gamma)^3-3*(2+gamma)-2))*[(2+gamma)^2-1 3+gamma 3+gamma;
-                                                 3+gamma (2+gamma)^2-1 3+gamma;
-                                                 3+gamma 3+gamma (2+gamma)^2-1];
-
-A = -R*Labc_inv;
-B = Labc_inv;
-u = vabc;
-d = we*phi_m*Labc_inv*[sin(theta_e);sin(theta_e-2*pi/3);sin(theta_e+2*pi/3)];
-
-x_dot = A * x + B * u + d;
-block.Derivatives.Data = x_dot;
 
 %endfunction
 
@@ -105,5 +106,16 @@ function Update(block)
 
 %   block.Dwork(1).Data = block.InputPort(1).Data;
   
+%endfunction
+
+% function SetInputPortSamplingMode(block, idx, fd)
+% 
+% block.InputPort(idx).SamplingMode = fd;
+% block.InputPort(idx).SamplingMode = fd;
+% 
+% block.OutputPort(1).SamplingMode = fd;
+% block.OutputPort(2).SamplingMode = fd;
+% block.OutputPort(3).SamplingMode = fd;
+
 %endfunction
 
--- spmsm/voltage_equation_abc.m
+++ spmsm/voltage_equation_abc.m
@@ -1,4 +1,4 @@
-function voltage_equation(block)
+function voltage_equation_abc(block)
 % Level-2 MATLAB file S-Function.
 
 %   Copyright 1990-2009 The MathWorks, Inc.
@@ -20,19 +20,19 @@ function setup(block)
   block.SetPreCompInpPortInfoToDynamic;
   block.SetPreCompOutPortInfoToDynamic;
  
-  block.InputPort(1).Dimensions        = [2,1];
+  block.InputPort(1).Dimensions        = [3,1];
   block.InputPort(1).DirectFeedthrough = false;
   
-  block.InputPort(2).Dimensions        = [1];
+  block.InputPort(2).Dimensions        = [2,1];
   block.InputPort(2).DirectFeedthrough = false;
   
-  block.OutputPort(1).Dimensions       = [2,1];
+  block.OutputPort(1).Dimensions       = [3,1];
   
   %% Set block sample time to continuous
   block.SampleTimes = [0 0];
   
   %% Setup Dwork
-  block.NumContStates = 2;
+  block.NumContStates = 3;
   
   %% Set the block simStateCompliance to default (i.e., same as a built-in block)
   block.SimStateCompliance = 'DefaultSimState';
@@ -62,7 +62,7 @@ function InitConditions(block)
 
 %% Initialize Dwork
 %   block.Dwork(1).Data = block.DialogPrm(1).Data;
-block.ContStates.Data = [0;0];
+block.ContStates.Data = [0;0;0];
   
 %endfunction
 
@@ -74,26 +74,30 @@ block.OutputPort(1).Data = block.ContStates.Data;
 
 function Derivative(block)
 
-vdq = block.InputPort(1).Data;
-wr = block.InputPort(2).Data;
-idq = block.ContStates.Data;
+vabc = block.InputPort(1).Data;
+theta_r = block.InputPort(2).Data(1);
+wr = block.InputPort(2).Data(2);
+x = block.ContStates.Data;
 pa = block.DialogPrm(1).Data;
-Ld = pa.Ld;
-Lq = pa.Lq;
 R = pa.R;
 P = pa.P;
+Lms = pa.Lms;
+Lls = pa.Lls;
 phi_m = pa.phi_m;
 we = P/2 * wr;
+theta_e = P/2 * theta_r;
+gamma = 2*Lls/Lms;
+Labc_inv = 2/(Lms*((2+gamma)^3-3*(2+gamma)-2))*[(2+gamma)^2-1 3+gamma 3+gamma;
+                                                 3+gamma (2+gamma)^2-1 3+gamma;
+                                                 3+gamma 3+gamma (2+gamma)^2-1];
 
-A = [-R/Ld we*Lq/Ld;
-     -we*Ld/Lq -R/Lq];
-B = [1/Ld 0;
-     0 1/Lq];
-u = vdq;
-d = -we*phi_m/Lq*[0;1];
+A = -R*Labc_inv;
+B = Labc_inv;
+u = vabc;
+d = we*phi_m*Labc_inv*[sin(theta_e);sin(theta_e-2*pi/3);sin(theta_e+2*pi/3)];
 
-idq_dot = A * idq + B * u + d;
-block.Derivatives.Data = idq_dot;
+x_dot = A * x + B * u + d;
+block.Derivatives.Data = x_dot;
 
 %endfunction
 
