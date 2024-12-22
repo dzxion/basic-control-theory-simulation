32feca7d5aaf42e59ba09742c591ed7bb53df121
dzxion <30489987+dzxion@users.noreply.github.com>
2024-12-22 22:48:49 +0800

    temp
    1.电流环闭环验证通过
    2.速度有界

--- pmsm/init.m
+++ pmsm/init.m
@@ -3,21 +3,21 @@ clc
 
 % plant
 pa.J = 0.1;
-pa.B = 0.0001;
+pa.B = 1;
 pa.Lms = 300e-6; % gap crossing inductance
 pa.Lls = 10e-6;% leakage inductance
 pa.Ldelta = 20e-6;
 Ls = 3/2*pa.Lms + pa.Lls;
-pa.Ld = Ls - 3/2*pa.Ldelta;
-pa.Lq = Ls + 3/2*pa.Ldelta;
-pa.phi_m = 0.6;
-pa.P = 6;% number of pole
-pa.R = 20e-3;
+% pa.Ld = Ls - 3/2*pa.Ldelta;
+% pa.Lq = Ls + 3/2*pa.Ldelta;
+pa.Lq = 2.39e-3;
+pa.Ld = 2.10e-3;
+pa.phi_m = 0.00469;
+pa.P = 14;% number of pole
+pa.R = 5.2;
 pa.dc = 24;
 pa.CarrFreq = 10;
 
-% pa.Lq = 2.39e-3;
-% pa.Ld = 2.10e-3;
 % pa.Kf = 0.2;
 % pa.Km = .015;
 % pa.Kb = .015;
